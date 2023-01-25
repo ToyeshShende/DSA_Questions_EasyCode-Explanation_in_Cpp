d80948f38a02da3b217d4cd54c5d2ea227d51efe
ToyeshShende <119116915+ToyeshShende@users.noreply.github.com>
2023-01-25 20:34:07 +0530

    Update and rename Duplicate_using_mapping.cpp to 6_Duplicate_using_mapping.cpp

--- 6_Duplicate_using_mapping.cpp
+++ 6_Duplicate_using_mapping.cpp
@@ -1,3 +1,6 @@
+https://www.codingninjas.com/codestudio/problems/find-unique_625159?source=youtube&campaign=love_babbar_codestudio1&utm_source=youtube&utm_medium=affiliate&utm_campaign=love_babbar_codestudio1&leftPanelTab=3
+
+
 int findDuplicate(vector<int> &arr) 
 {
     // Write your code here
