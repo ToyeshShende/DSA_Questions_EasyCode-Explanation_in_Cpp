257824daaa976d96edd71b476d34e0aac17f32da
ToyeshShende <119116915+ToyeshShende@users.noreply.github.com>
2023-01-29 19:55:15 +0530

    Create 18_Rotate _array.cpp

---
+++ 18_Rotate _array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        vector<int>v;
+        if(k>nums.size()){
+            k=k%nums.size();      //This condition is must to remember because of they give k greater than size then it will give run time error 
+        }
+        for(int i=nums.size()-k;i<nums.size();i++){
+            v.push_back(nums[i]);  //push last k elements
+
+        }
+        for(int i=0;i<nums.size()-k;i++){
+            v.push_back(nums[i]);  //push remaining elements
+        }
+        nums.clear();   //delete all elements from orginal vector
+        copy(v.begin(),v.end(),back_inserter(nums));  // copy all elements of v in to original num verctor
+    }
+};
