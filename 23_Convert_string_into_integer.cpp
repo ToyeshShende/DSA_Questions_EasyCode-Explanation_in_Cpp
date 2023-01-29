a242b208d002eacd075a4e4fbd056117ec25f5f5
ToyeshShende <119116915+ToyeshShende@users.noreply.github.com>
2023-01-29 20:24:23 +0530

    Create 23_Convert_string_into_integer.cpp

---
+++ 23_Convert_string_into_integer.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include<bits/stdc++.h>
+
+
+using namespace std;
+
+int main()
+{
+    string s="1001";
+    string s2="1011";
+    int I1=stoi(s);
+    int I2=stoi(s2);
+    cout<<I1+I2;
+
+    return 0;
+}
