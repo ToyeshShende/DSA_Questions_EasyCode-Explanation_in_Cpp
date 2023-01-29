ed581258035f20f9e8001d21bd1adee423a7dd22
ToyeshShende <119116915+ToyeshShende@users.noreply.github.com>
2023-01-29 23:20:48 +0530

    Create 24_Count_Prime_numbers.cpp

---
+++ 24_Count_Prime_numbers.cpp
@@ -0,0 +1,26 @@
+
+
+Sieve of Eratosthenes
+
+
+
+https://leetcode.com/problems/count-primes/solutions/
+
+
+class Solution {
+public:
+    int countPrimes(int n) {
+        vector<bool>v(n+1,true);
+        int count=0;
+        v[0]=v[1]=false;
+        for(int i=2;i<n;i++){
+            if(v[i]){
+                count++;
+            }
+            for(int j=2*i;j<n;j=j+i){    //table of 2 3 5 7 etc.
+                v[j]=0;
+            }
+        }
+        return count;
+    }
+};
