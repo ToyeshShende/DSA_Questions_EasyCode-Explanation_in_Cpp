ed01c7429d66622a200682a0fdcda1fdde21a784
ToyeshShende <119116915+ToyeshShende@users.noreply.github.com>
2023-02-03 00:22:15 +0530

    Create macro_define.cpp

---
+++ macro_define.cpp
@@ -0,0 +1,13 @@
+  
+#include <iostream>
+
+using namespace std;
+#define Pi 3.14     //pre define something and use in code directly (doesn't take space)
+int main()
+{
+    int r=5;
+    int area=Pi*r*r;
+    cout<<area;
+
+    return 0;
+}
