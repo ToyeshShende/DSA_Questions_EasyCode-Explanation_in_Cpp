b7787b9bc1b0c1891afcfd97844f5c52e1bf7b42
ToyeshShende <119116915+ToyeshShende@users.noreply.github.com>
2023-02-02 23:17:24 +0530

    Update 43_dynamic_array_allocation.cpp

--- 43_dynamic_array_allocation.cpp
+++ 43_dynamic_array_allocation.cpp
@@ -24,6 +24,13 @@ int main()
     for(int i=0;i<n;i++){
         cout<<arr1[i]<<" ";
     }
+    int j=6;
+    while(j>0){
+        int *arr2=new int [3];
+        
+        // after using it delete arr2
+        delete []arr2;
+    }
     
     return 0;
 }
