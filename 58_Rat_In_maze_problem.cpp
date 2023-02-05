9cb51f9ae0d933de7d21e3c7f89460f382b859f7
ToyeshShende <119116915+ToyeshShende@users.noreply.github.com>
2023-02-05 12:06:31 +0530

    Update 58_Rat_In_maze_problem.cpp

--- 58_Rat_In_maze_problem.cpp
+++ 58_Rat_In_maze_problem.cpp
@@ -71,3 +71,43 @@ public:
         sort(final.begin(),final.end());
         return final;
     }
+
+
+
+
+// more simplyfied code
+
+
+
+
+void solve(vector<string>&ans,vector<vector<int>>m,int n,int x,int y ,vector<vector<int>>visited,string out){
+        
+        if(x<0 || x>n-1 ||y<0 || y>n-1 || m[x][y]==0 || visited[x][y]==1){
+            return ;
+        }
+        
+        if(x==n-1 && y==n-1){
+            ans.push_back(out);
+            return;
+        }
+        visited[x][y]=1;
+        solve(ans,m,n,x+1,y,visited,out+'D');  //down
+        solve(ans,m,n,x,y-1,visited,out+'L');  //left
+        solve(ans,m,n,x,y+1,visited,out+'R');  //right
+        solve(ans,m,n,x-1,y,visited,out+'U');  //up
+        visited[x][y]=0;  //back tracking
+    }
+    vector<string> findPath(vector<vector<int>> &m, int n) {
+        // Your code goes here
+        vector<string>ans;
+         vector<vector<int>>visited=m;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                visited[i][j]=0;
+            }
+        }
+        string out="";
+        solve(ans,m,n,0,0,visited,out);
+        sort(ans.begin(),ans.end());
+        return ans;
+    }
