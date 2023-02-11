940a96821e3203f8c23aee9955f153f7e91b3910
ToyeshShende <119116915+ToyeshShende@users.noreply.github.com>
2023-02-11 11:37:01 +0530

    Create 62_consturctor_overloading&parametrized_constructor.cpp

---
+++ 62_consturctor_overloading&parametrized_constructor.cpp
@@ -0,0 +1,30 @@
+
+#include <iostream>
+using namespace std;
+class Hero {
+    int health;
+    char level;
+    public: 
+    //constructor overloading
+    Hero(int health){
+        this->health=health;  // we use this->health because if use this so there is no need to declare a new variable and store health in it 
+    }
+    Hero(int health,char level){  //paramatric contructor
+        this->health=health;
+        this->level=level;
+    }
+    void print(){
+        cout<<health<<endl;
+        cout<<level<<endl;
+    }
+};
+int main()
+{   Hero *h1=new Hero(23);
+    h1->print();
+    Hero *h2=new Hero(24,'n');
+    h2->print();
+    
+    
+
+    return 0;
+}
