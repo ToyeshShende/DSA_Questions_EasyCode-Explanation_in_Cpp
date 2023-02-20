42282af5f1f0278163f163879550f8b29be60f5a
ToyeshShende <119116915+ToyeshShende@users.noreply.github.com>
2023-02-20 21:50:03 +0530

    Update 84_minimum_add_to_make_parenthesis_valid.cpp

--- 84_minimum_add_to_make_parenthesis_valid.cpp
+++ 84_minimum_add_to_make_parenthesis_valid.cpp
@@ -30,3 +30,45 @@ public:
         
     }
 };
+
+
+https://www.codingninjas.com/codestudio/problems/minimum-cost-to-make-string-valid_1115770?leftPanelTab=0&campaign=Lovebabbarcodestudio&utm_source=youtube&utm_medium=affiliate&utm_campaign=Lovebabbarcodestudio
+
+#include <bits/stdc++.h> 
+int findMinimumCost(string str) {
+  // Write your code here
+  if(str.length()%2==1){
+    return -1;
+  }
+  stack<char>s;
+  for(int i=0;i<str.size();i++){
+      if(str[i]=='{'){
+        s.push(str[i]);
+      }
+      else if(!s.empty() && str[i]=='}'){
+        if(s.top()=='{'){
+          s.pop();
+        }
+        else{
+          s.push(str[i]);
+        }
+      }
+      else{
+        s.push(str[i]);
+      }
+
+  }
+  int a=0;
+  int b=0;
+  while(!s.empty()){
+    if(s.top()=='{'){
+      a++;
+    }
+    else{
+      b++;
+    }
+    s.pop();
+  }
+  return (a+1)/2 + (b+1)/2;
+
+}
