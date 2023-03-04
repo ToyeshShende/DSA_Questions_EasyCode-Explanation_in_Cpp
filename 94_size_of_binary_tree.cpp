b87dba83bb2d01f62318180fe1bd9affe56d9773
ToyeshShende <119116915+ToyeshShende@users.noreply.github.com>
2023-03-04 21:18:00 +0530

    Create 94_size_of_binary_tree.cpp

---
+++ 94_size_of_binary_tree.cpp
@@ -0,0 +1,23 @@
+https://leetcode.com/problems/count-complete-tree-nodes/submissions/908975383/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+        if(root==NULL){
+            return 0;
+        }
+        return (countNodes(root->left)+1+countNodes(root->right));
+        
+    }
+};
