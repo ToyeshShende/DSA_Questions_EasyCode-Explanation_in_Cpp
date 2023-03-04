71e9a5b809bb213ab72808435d4c4b4016a66acb
ToyeshShende <119116915+ToyeshShende@users.noreply.github.com>
2023-03-04 23:53:34 +0530

    Create 96_diameter_of_binary_tree.cpp

---
+++ 96_diameter_of_binary_tree.cpp
@@ -0,0 +1,30 @@
+https://practice.geeksforgeeks.org/batch/dsa-4/track/DSASP-Tree/problem/diameter-of-binary-tree
+
+
+
+//  maximum diameter found in   left side of binary tree
+                          //    right side of binary tree
+                        //      found in both side merge of two which containg root
+
+
+class Solution {
+  public:
+  int height(Node *root){
+      if(root==NULL){
+          return 0;
+      }
+      int lefty=height(root->left);
+      int righty=height(root->right);
+      return max(lefty,righty)+1;
+  }
+    
+    int diameter(Node* root) {
+        if(root==NULL){
+            return 0;
+        }
+        int a=diameter(root->left);
+        int b=diameter(root->right);
+        int c= (height(root->left)+1+ height(root->right));;
+        return max(a,max(b,c));
+    }
+};
