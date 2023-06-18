93878b12735aef9c43e2d261ccd7f94610716328
ToyeshShende <119116915+ToyeshShende@users.noreply.github.com>
2023-06-18 19:58:41 +0530

    Update 13_Painter's_partition.cpp

--- 13_Painter's_partition.cpp
+++ 13_Painter's_partition.cpp
@@ -1,3 +1,4 @@
+
 Question link
 https://www.codingninjas.com/codestudio/problems/painter's-partition-problem_1089557?source=youtube&campaign=love_babbar_codestudio2&utm_source=youtube&utm_medium=affiliate&utm_campaign=love_babbar_codestudio2&leftPanelTab=1
 
@@ -40,4 +41,46 @@ int findLargestMinDistance(vector<int> &boards, int k)
         }
     }
     return sum;
+}
+
+
+agressive cow
+
+#include <bits/stdc++.h>
+bool possible(int mid, vector<int> &stalls, int k) {
+    int lastposi=stalls[0];
+    int count=1;
+    for(int i=0;i<stalls.size();i++){
+        if(stalls[i]-lastposi>=mid){
+            count++;
+            if(count==k){
+                return true;
+            }
+            lastposi=stalls[i];
+        }
+    }
+    return false;
+}
+int aggressiveCows(vector<int> &stalls, int k)
+{   sort(stalls.begin(),stalls.end());
+    int low=0;
+    int high=-1;
+    for (int i = 0; i < stalls.size(); i++) {
+        high=max(high,stalls[i]);
+    }
+    int ans=0;
+    while(low<=high){
+        int mid=(low+high)/2;
+        if(possible(mid,stalls,k)){
+            low=mid+1;
+             ans=mid;
+            
+
+        }
+        else{
+            high=mid-1;
+        }
+
+    }
+    return ans;
 }
