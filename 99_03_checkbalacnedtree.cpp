e47625c7ae27fcdccfe56270e77b8bfa504026d8
ToyeshShende <119116915+ToyeshShende@users.noreply.github.com>
2023-07-02 14:37:31 +0530

    Update 99_03_checkbalacnedtree.cpp

--- 99_03_checkbalacnedtree.cpp
+++ 99_03_checkbalacnedtree.cpp
@@ -18,3 +18,39 @@
         return false;
 
     }
+
+
+
+
+
+
+int bal(TreeNode* root){
+        if(root==NULL){
+            return 0;
+        }
+        int left=bal(root->left);
+        if(left==-1){
+            return -1;
+        }
+        int right=bal(root->right);
+        if(right==-1){
+            return -1;
+        }
+        if(abs(right-left)>1){
+            return -1;
+        }
+        else{
+            return max(left,right)+1;
+        }
+    }
+    bool isBalanced(TreeNode* root) {
+        if(root==NULL){
+            return true;
+        }
+        int flag=bal(root);
+        if(flag==-1){
+            return false;
+        }
+        return true;
+    }
+};
