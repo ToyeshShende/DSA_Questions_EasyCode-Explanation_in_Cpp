e3b55687f03d47e9496acd0a982b777d5400f965
ToyeshShende <119116915+ToyeshShende@users.noreply.github.com>
2023-07-12 00:32:15 +0530

    Create 99_07_inorder,postorder,preorder.cpp

---
+++ 99_07_inorder,postorder,preorder.cpp
@@ -0,0 +1,7 @@
+for inorder transversal 
+
+                       5
+                     3   7
+                Null   4 6  10              start from 5 to left 3 then NULL so print 3 to right 4 then 4's left is null so print 4  return to 3 return to 5 print 5
+                                                         to right 7 left is 6 then 6's left is null so print 6 then return to 7 print 7 then move to 10 print 10 
+                                                                  
