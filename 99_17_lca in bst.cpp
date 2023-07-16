dd96a97cf46a22ae5e0a7dd1b69f7befaba559c7
ToyeshShende <119116915+ToyeshShende@users.noreply.github.com>
2023-07-16 11:20:40 +0530

    Create 99_17_lca in bst.cpp

---
+++ 99_17_lca in bst.cpp
@@ -0,0 +1,18 @@
+
+https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+       if(root==NULL){
+           return NULL;
+       }
+       
+    if(root->val>p->val && root->val>q->val){
+            return lowestCommonAncestor(root->left, p,q);
+        }
+    else if(root->val<p->val && root->val<q->val){
+            return lowestCommonAncestor(root->right, p,q);
+        }
+    else{
+            return root;
+        }
+       
+    }
