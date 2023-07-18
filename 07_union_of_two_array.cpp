91481304737413f940f4953b3a86952cb723cbbc
ToyeshShende <119116915+ToyeshShende@users.noreply.github.com>
2023-07-18 22:41:50 +0530

    Update 07_union_of_two_array.cpp

--- 07_union_of_two_array.cpp
+++ 07_union_of_two_array.cpp
@@ -16,6 +16,52 @@ vector<int> findArrayIntersection(vector<int> &arr1, int n, vector<int> &arr2, i
 
 	
 }
+optimum approach
+ vector<int> findUnion(int arr1[], int arr2[], int n, int m)
+    {
+        int i=0;
+        int j=0;
+        vector<int>v;
+        
+        while(i<n && j<m){
+            if(i>0 && arr1[i]==arr1[i-1]){
+                i++;
+                continue;
+            }
+            if(j>0 && arr2[j]==arr2[j-1]){
+                j++;
+                continue;
+            }
+            if(arr1[i]<arr2[j]){
+                v.push_back(arr1[i++]);
+            }
+            else if(arr1[i]>arr2[j]){
+                v.push_back(arr2[j++]);
+            }
+            else{
+                //equal
+                v.push_back(arr1[i++]);
+                j++;
+            }
+        }
+        while(i<n){
+             if(i>0 && arr1[i]==arr1[i-1]){
+                i++;
+                continue;
+            }
+            if(i<n){
+            v.push_back(arr1[i++]);}
+        }
+        while(j<m){
+            if(j>0 && arr2[j]==arr2[j-1]){
+                j++;
+                continue;
+            }
+            if(j<m){
+            v.push_back(arr2[j++]);}
+        }
+        return v;
+    }
 
 
 
