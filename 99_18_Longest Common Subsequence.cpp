3f001c4b012f523217a27199a0410bc3da1be48c
ToyeshShende <119116915+ToyeshShende@users.noreply.github.com>
2023-08-17 10:31:07 +0530

    Create 99_18_Longest Common Subsequence.cpp

---
+++ 99_18_Longest Common Subsequence.cpp
@@ -0,0 +1,19 @@
+
+https://cleetcode.com/problems/longest-common-subsequence/description/
+class Solution {
+public:
+    int solve(string &text1,string &text2,int i,int j,vector<vector<int>>&dp){
+        if(i<0 || j<0){
+            return 0;
+        }
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(text1[i]==text2[j]) return dp[i][j]=1+solve(text1,text2,i-1,j-1,dp);
+        else return dp[i][j]=max(solve(text1,text2,i-1,j,dp),solve(text1,text2,i,j-1,dp));
+
+    }
+    int longestCommonSubsequence(string text1, string text2) {
+        vector<vector<int>>dp(text1.size(),vector<int>(text2.size(),-1));
+        return solve(text1,text2,text1.size()-1,text2.size()-1,dp);
+    }
+};
+
