d09c8880171cbe2846c40737340709bb7ad34cf9
ToyeshShende <119116915+ToyeshShende@users.noreply.github.com>
2023-08-18 11:22:41 +0530

    Update 57_permutations.cpp

--- 57_permutations.cpp
+++ 57_permutations.cpp
@@ -42,3 +42,25 @@ public:
 
     }
 };
+
+
+next permutation:https://leetcode.com/problems/next-permutation/description/
+ void nextPermutation(vector<int>& nums) {
+        int idx=-1;
+        int n=nums.size();
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                idx=i;
+                break;
+            }
+        }
+        // 1 2 3
+        for(int i=n-1 ;i>=idx && idx!=-1 ;i--){
+            if(nums[i]>nums[idx]){
+                swap(nums[i],nums[idx]);
+                break;
+            }
+        }
+        reverse(nums.begin()+1+idx,nums.end());
+        
+    }
